0b707e1a8f63ab3bd87421576a3da33ff518fa1d
palumbolorenzo <palumbolorenzo97@gmail.com>
2022-11-28 16:10:15 +0100

    provvisorio

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bootstrap">
+    <button class="btn btn-primary"></button>
+    <HeaderPage />
+    <MainPage />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderPage from '@/components/HeaderPage.vue';
+import MainPage from '@/components/MainPage.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HeaderPage,
+    MainPage,
+  },
+};
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style>
+@import '~bootstrap/dist/css/bootstrap.css';
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/HeaderPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <img src="@/img/logo-small.svg" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+name: 'HeaderPage'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
